76ed82cbbc8007ec48ae07d3da6b9dd61bed58ea
hungluongduc@sohagame.vn <hungluongduc@sohagame.vn>
2017-10-20 08:50:17 +0700

    56

--- wp-content/themes/gardensoft/page-blank.php
+++ wp-content/themes/gardensoft/page-blank.php
@@ -272,20 +272,4 @@ get_header(); ?>
 <?php do_action('flatsome_after_page'); ?>
 
 <?php get_footer(); ?>
-<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-<script>
-    $(".faq-q").click( function () {
-        var container = $(this).parents(".faq-c");
-        var answer = container.find(".faq-a");
-        var trigger = container.find(".faq-t");
 
-        answer.slideToggle(200);
-
-        if (trigger.hasClass("faq-o")) {
-            trigger.removeClass("faq-o");
-        }
-        else {
-            trigger.addClass("faq-o");
-        }
-    });
-</script>
