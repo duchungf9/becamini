e0a4d8a8c3871da4d29929e463153664f93a668a
hungluongduc@sohagame.vn <hungluongduc@sohagame.vn>
2017-10-20 08:27:16 +0700

    56

--- wp-content/themes/gardensoft/page-blank.php
+++ wp-content/themes/gardensoft/page-blank.php
@@ -272,20 +272,20 @@ get_header(); ?>
 <?php do_action('flatsome_after_page'); ?>
 
 <?php get_footer(); ?>
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script>
-    $(".faq-q").click( function () {
-        var container = $(this).parents(".faq-c");
-        var answer = container.find(".faq-a");
-        var trigger = container.find(".faq-t");
-
-        answer.slideToggle(200);
-
-        if (trigger.hasClass("faq-o")) {
-            trigger.removeClass("faq-o");
-        }
-        else {
-            trigger.addClass("faq-o");
-        }
-    });
-</script>
+<!--<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>-->
+<!--<script>-->
+<!--    $(".faq-q").click( function () {-->
+<!--        var container = $(this).parents(".faq-c");-->
+<!--        var answer = container.find(".faq-a");-->
+<!--        var trigger = container.find(".faq-t");-->
+<!---->
+<!--        answer.slideToggle(200);-->
+<!---->
+<!--        if (trigger.hasClass("faq-o")) {-->
+<!--            trigger.removeClass("faq-o");-->
+<!--        }-->
+<!--        else {-->
+<!--            trigger.addClass("faq-o");-->
+<!--        }-->
+<!--    });-->
+<!--</script>-->
